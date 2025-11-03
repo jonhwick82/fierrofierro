8d0db9d8af65b286e5692ceb1857e0c425245a68
jonhwick82 <reichelj82@gmail.com>
2025-11-03 17:59:13 -0300

    ss
    nn

--- lib/proyect/pantalla_dashboard.dart
+++ lib/proyect/pantalla_dashboard.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:fl_chart/fl_chart.dart';
-import 'package:intl/intl.dart';
+///import 'package:intl/intl.dart';
 
 /// Un modelo simple para agrupar las estadísticas calculadas.
 class DashboardStats {
